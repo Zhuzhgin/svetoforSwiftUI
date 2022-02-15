f2e1d70939cc2d61ea15db010a455fbd3ad382ba
Zhuzhgin <artemfox@list.ru>
2022-02-15 21:57:50 +0500

    Initial Commit

---
+++ svetoforSwiftUI/svetoforSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  svetoforSwiftUIApp.swift
+//  svetoforSwiftUI
+//
+//  Created by Artem Zhuzhgin on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct svetoforSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
